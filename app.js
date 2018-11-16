32e74d9afaa8ce0882c2ebb26729c321cd8d3c3d
EquationKid <equationkid@gmail.com>
2018-11-16 16:21:03 +1000

    Wrote todos and added extra ejs files

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-"" 
+const express = require('express'),
+        app = express(),
+        io = require('socket.io')
+        bodyParser = require('body-parser')
+
+// TODO: app.get
+// TODO: app.post
+// TODO: io
