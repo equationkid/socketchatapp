f6d1227e2196701df20365f604d5c117a61073fb
Equation Kid <equationkid@gmail.com>
2018-11-16 16:41:16 +1000

    Added app.get in app.js. Made the html for some of the views

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
 const express = require('express'),
         app = express(),
-        io = require('socket.io')
-        bodyParser = require('body-parser')
+        io = require('socket.io'),
+        bodyParser = require('body-parser'),
+        server = require('http').Server(app)
 
-// TODO: app.get
+app.use('/cdn', express.static)
+app.use(bodyParser.urlencoded({
+        extended: true
+}))
+
+app.get('/', (req, res) => {
+        res.send('home')
+})
+app.get('/newroom', (req, res) => {
+        res.send('newroom')
+})
+app.get('/rooms', (req, res) => {
+        res.send('rooms')
+})
 // TODO: app.post
 // TODO: io
